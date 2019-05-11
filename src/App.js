bf8f11dc50f3047de4ac8e807e6b7946e88d95a3
sadief <sadiefreeman@gmail.com>
2019-05-11 00:18:40 -0700

    Refactor auth to pull out of componentdidmount

--- src/App.js
+++ src/App.js
@@ -2,21 +2,41 @@ import React, { Component } from 'react';
 import NavBar from './NavBar/NavBar';
 import Streams from './Streams/Streams';
 import Stream from './Stream/Stream';
-import { Route } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
 import Callback from './Callback';
 import SecuredRoute from './SecuredRoute/SecuredRoute';
+import auth0Client from './Auth.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkingSession: true,
+    }
+  }
+  async componentDidMount() {
+    if (this.props.location.pathname === '/callback') {
+      this.setState({ checkingSession: false });
+      return;
+    }
+    try {
+      await auth0Client.silentAuth();
+      this.forceUpdate();
+    } catch (err) {
+      if (err.error !== 'login_required') console.log(err.error);
+    }
+    this.setState({ checkingSession: false });
+  }
   render() {
     return (
       <div>
         <NavBar />
         <Route exact path='/' component={Streams} />
-        <SecuredRoute path='/video/:id' component={Stream} />
+        <Route path='/video/:id' component={Stream} />
         <Route exact path='/callback' component={Callback} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
